352d2a9d96ed55b9f900a208d6179aee6de7535c
matin04 <fathuddinzodamatin@gmail.com>
2025-10-11 08:16:20 +0300

    add categories

--- menuapp/urls.py
+++ menuapp/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
-from .views import RegisterView, EmailConfirmView, LoginView, LogoutView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('dishes', DishViewSet)
+router.register('reviews', ReviewViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('confirm-email/<uuid:token>/', EmailConfirmView.as_view(), name='confirm_email'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('', include(router.urls)),
 ]
-
