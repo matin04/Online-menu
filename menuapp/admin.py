f2ce26015fbcae561ed2f0b64835feb7043844c8
matin04 <fathuddinzodamatin@gmail.com>
2025-10-11 04:39:32 +0300

    Register, Login Logaut i nekotorikh izminenii i CustomUser

--- menuapp/admin.py
+++ menuapp/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([User, Category, Dish, Review])
+admin.site.register([CustomUser, Category, Dish, Review])
--- menuapp/views.py
+++ menuapp/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from .models import CustomUser, EmailConfirmation
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import  logout
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+    
+
+class EmailConfirmView(APIView):
+    def get(self, request, token):
+        try:
+            confirm = EmailConfirmation.objects.get(token = token)
+            user = confirm.user
+            user.is_active = True
+            user.save()
+            confirm.delete()
+            return Response({'detail':'Email успешно подтвержден'}, status=status.HTTP_200_OK)
+        except EmailConfirmation.DoesNotExist:
+            return Response({'detail':'Неверный или устаревший токен'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user) 
+            refresh['email'] = user.email
+
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': {
+                    'id': user.id,
+                    'email': user.email
+                }
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({'message': 'Logged out successfully!'}, status=200)
+
